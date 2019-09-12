fad36c2fac27eba3e27c6da9bb6791791fe5736c
DoMinhHieu99 <dohieu19999@gmail.com>
2019-09-12 15:38:31 +0700

    Initial commit

---
+++ app/src/main/java/dohieu/com/librarytest/MainActivity.java
@@ -0,0 +1,14 @@
+package dohieu.com.librarytest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
